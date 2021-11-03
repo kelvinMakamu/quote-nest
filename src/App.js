f4ba2fc8854aea1d255d09fdc3ba59574834b25b
Kelvin Makamu <makamu3pmkelvin@gmail.com>
2021-11-03 21:57:37 +0300

    Adds quotes routes

--- src/App.js
+++ src/App.js
@@ -1,8 +1,24 @@
+import { Switch,Route } from "react-router-dom";
+import QuoteItem from "./components/quotes/QuoteItem";
+import QuoteList from './components/quotes/QuoteList';
+import QuoteForm from './components/quotes/QuoteForm';
+
+
 function App() {
   return (
-    <div>
-
-    </div>
+    <main>
+      <Switch>
+        <Route path="/quotes" exact>
+          <QuoteList/>
+        </Route>
+        <Route path="/quotes/:quoteId">
+          <QuoteItem/>
+        </Route>
+        <Route path="/quotes/new-quote" exact>
+          <QuoteForm/>
+        </Route>
+      </Switch>
+    </main>
   );
 }
 
