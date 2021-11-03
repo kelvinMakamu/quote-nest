411f2f025d224aaf14b9f0dc2f6b44da71ca22f1
Kelvin Makamu <makamu3pmkelvin@gmail.com>
2021-11-03 22:02:58 +0300

    Adds comments routes

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import { Switch,Route } from "react-router-dom";
 import QuoteItem from "./components/quotes/QuoteItem";
 import QuoteList from './components/quotes/QuoteList';
 import QuoteForm from './components/quotes/QuoteForm';
+import CommentsList from "./components/comments/CommentsList";
+import CommentItem from "./components/comments/CommentItem";
+import NewCommentForm from "./components/comments/NewCommentForm";
 
 
 function App() {
   return (
-    <main>
       <Switch>
         <Route path="/quotes" exact>
           <QuoteList/>
@@ -17,8 +19,17 @@ function App() {
         <Route path="/quotes/new-quote" exact>
           <QuoteForm/>
         </Route>
+
+        <Route path="/comments" exact>
+          <CommentsList/>
+        </Route>
+        <Route path="/comments/:commentId">
+          <CommentItem/>
+        </Route>
+        <Route path="/comments/new-comment" exact>
+          <NewCommentForm/>
+        </Route>
       </Switch>
-    </main>
   );
 }
 
