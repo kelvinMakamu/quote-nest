9330ab53908407f59b5037330d5934f10d8d670d
Kelvin Makamu <makamu3pmkelvin@gmail.com>
2021-11-03 23:02:04 +0300

    Adds dummy quotes for AllQuotes page component

--- src/pages/AllQuotes.js
+++ src/pages/AllQuotes.js
@@ -1,7 +1,17 @@
+import QuoteList from "../components/quotes/QuoteList";
+
+const DUMMY_QUOTES = [
+    {id:'q1',author:'Kelvin', text:'A quick brown fox jumped over the lazy dogs'},
+    {id:'q2',author:'Justin', text:'A Crated brown fox jumped over the lazy dogs'},
+    {id:'q3',author:'Waithero', text:'A curated brown fox jumped over the lazy dogs'},
+    {id:'q4',author:'NJambi', text:'A tailored brown fox jumped over the lazy dogs'},
+];
+
 const AllQuotes = () => {
-    return (
-        <h2>All Quotes Page</h2>
-    )
+    return <>
+            <h2>All Quotes Page</h2>
+            <QuoteList quotes={DUMMY_QUOTES}/>
+         </>
 }
 
 export default AllQuotes;
