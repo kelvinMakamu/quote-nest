0f297aa8151a579ece13d95f9edbbc62b9f51307
Kelvin Makamu <makamu3pmkelvin@gmail.com>
2021-11-03 23:37:53 +0300

    Adds not found page

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Layout from "./components/layout/Layout";
 import AllQuotes from "./pages/AllQuotes";
 import NewQuote from "./pages/NewQuote";
 import QuoteDetail from "./pages/QuoteDetail";
+import NotFound from "./pages/NotFound";
 
 
 function App() {
@@ -20,6 +21,9 @@ function App() {
         </Route>
         <Route path="/new-quote" exact>
           <NewQuote/>
+        </Route>
+        <Route path="*">
+          <NotFound/>
         </Route>
       </Switch>
     </Layout>
--- src/pages/QuoteDetail.js
+++ src/pages/QuoteDetail.js
@@ -2,14 +2,26 @@ import { Route, useParams } from "react-router-dom";
 import Comments from "../components/comments/Comments";
 import HighlightedQuote from "../components/quotes/HighlightedQuote";
 
+const DUMMY_QUOTES = [
+    {id:'q1',author:'Kelvin', text:'A quick brown fox jumped over the lazy dogs'},
+    {id:'q2',author:'Justin', text:'A Crated brown fox jumped over the lazy dogs'},
+    {id:'q3',author:'Waithero', text:'A curated brown fox jumped over the lazy dogs'},
+    {id:'q4',author:'NJambi', text:'A tailored brown fox jumped over the lazy dogs'},
+];
+
 const QuoteDetail = () => {
 
     const params = useParams();
 
+    const quote = DUMMY_QUOTES.find(quote =>quote.id=== params.quoteId);
+
+    if(!quote){
+        return <p>No quote found</p>
+    }
+
     return (
         <>
-            <h2>Quote detail Page</h2>
-            <p>{params.quoteId}</p>
+            <HighlightedQuote author={quote.author} text={quote.text}/>
             <Route path={`/quotes/${params.quoteId}/comments`}>
               <Comments/>
             </Route>
